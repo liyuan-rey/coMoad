8c64b92c3846a57b1fbd0f1307722eed52cabe63
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2004-12-20 12:35:00 +0000

    git-svn-id: svn://localhost/coMoad/trunk@8 5bfb4c0d-860c-1a49-92c4-fb21056c953a

--- server.bat
+++ server.bat
@@ -1,5 +1,3 @@
 @echo off
 
-call cpset.bat
-
-%JAVA_PATH%\bin\java.exe -Xms128m -Xmx512m -server -classpath %DYNAPATH%;D:\MyProjs\eclipse_workspace\DFEMPDM\dyna.jar dyna.framework.Server
+java.exe -Xms128m -Xmx512m -server -cp .;lib\920classes12.zip;lib\920nls_charset12.zip;lib\activation.jar;lib\animation.jar;lib\binding.jar;lib\charts.jar;lib\crimson.jar;lib\forms-extra.zip;lib\forms.jar;lib\jg-uif.jar;lib\looks-all.jar;lib\mail.jar;lib\NIST.zip;lib\script.zip dyna.framework.Server
