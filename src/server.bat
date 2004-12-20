21f6c8fbd54fee71a701a4d8d17bc72701addee4
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2004-12-20 13:41:05 +0000

    git-svn-id: svn://localhost/coMoad/trunk@40 5bfb4c0d-860c-1a49-92c4-fb21056c953a

--- src/server.bat
+++ src/server.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java.exe -Xms128m -Xmx512m -server -cp .;lib\920classes12.zip;lib\920nls_charset12.zip;lib\activation.jar;lib\animation.jar;lib\binding.jar;lib\charts.jar;lib\crimson.jar;lib\forms-extra.zip;lib\forms.jar;lib\jg-uif.jar;lib\looks-all.jar;lib\mail.jar;lib\NIST.zip;lib\script.zip dyna.framework.Server
+java.exe -Xms128m -Xmx512m -server -cp .;..\lib\920classes12.zip;..\lib\920nls_charset12.zip;..\lib\activation.jar;..\lib\animation.jar;..\lib\binding.jar;..\lib\charts.jar;..\lib\crimson.jar;..\lib\forms-extra.zip;..\lib\forms.jar;..\lib\jg-uif.jar;..\lib\looks-all.jar;..\lib\mail.jar;..\lib\NIST.zip;..\lib\script.zip dyna.framework.Server
